74f462d74c598b4c6e70a2363b50e8b567863f67
departever <609336902@qq.com>
2025-03-05 15:01:13 +0800

    Revert "修复了mv页面tab点击的bug，新增代码压缩~"
    This reverts commit ed1ea823beb23c597f21c86e8a62e38e2329af1f.

--- src/page/mvs/index.vue
+++ src/page/mvs/index.vue
@@ -2,20 +2,20 @@
     <div class="mvs" ref="page">
         <div class="tabs-wrap">
             <span class="tabs-type">地区：</span>
-            <Tabs :tabs="areaTabs" class="tabs" type="split" :active="activeAreaTabIndex" @tabChange="activeAreaTabIndex = $event"/>
+            <Tabs :tabs="areaTabs" class="tabs" type="split" v-model="activeAreaTabIndex" />
         </div>
         <div class="tabs-wrap">
             <span class="tabs-type">类型：</span>
-            <Tabs :tabs="typeTabs" class="tabs" type="split" :active="activeTypeTabIndex" @tabChange="activeTypeTabIndex = $event"/>
+            <Tabs :tabs="typeTabs" class="tabs" type="split" v-model="activeTypeTabIndex" />
         </div>
         <div class="tabs-wrap">
             <span class="tabs-type">排序：</span>
-            <Tabs :tabs="sortTabs" class="tabs" type="split" :active="activeSortTabIndex" @tabChange="activeSortTabIndex = $event"/>
+            <Tabs :tabs="sortTabs" class="tabs" type="split" v-model="activeSortTabIndex" />
         </div>
-        <WithPagination :getData="getAllMvs" :getDataParams="getDataParams" :limit="40" :scrollTarget="page"
+        <WithPagination :getData="getAllMvs" :getDataParams="getDataParams()" :limit="40" :scrollTarget="scrollTarget"
             :total="mvCount" @getDataSuccess="onGetMvs">
             <ul class="list-wrap">
-                <li v-for="mv in mvs" :key="mv.id" class="list-item">
+                <li :key="mv.id" class="list-item" v-for="mv in mvs">
                     <MvCard :author="mv.artistName" :duration="mv.duration" :id="mv.id" :img="mv.cover" :name="mv.name"
                         :playCount="mv.playCount" />
                 </li>
@@ -25,38 +25,38 @@
 </template>
 
 <script setup>
-import { computed, ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { getAllMvs } from "@/api";
 import MvCard from "@/components/mv-card.vue";
 import WithPagination from "@/components/with-pagination.vue";
 
-const areaTabs = ["全部", "内地", "港台", "欧美", "日本", "韩国"]
-const typeTabs = ["全部", "官方版", "原声", "现场版", "网易出品"]
-const sortTabs = ["上升最快", "最热", "最新"]
+const areaTabs = ["全部", "内地", "港台", "欧美", "日本", "韩国"];
+const typeTabs = ["全部", "官方版", "原声", "现场版", "网易出品"];
+const sortTabs = ["上升最快", "最热", "最新"];
 
-const mvs = ref([])
-const mvCount = ref(0)
-const activeAreaTabIndex = ref(0)
-const activeTypeTabIndex = ref(0)
-const activeSortTabIndex = ref(0)
-const page = ref(null)
+const activeAreaTabIndex = ref(0);
+const activeTypeTabIndex = ref(0);
+const activeSortTabIndex = ref(0);
+const mvs = ref([]);
+const mvCount = ref(0);
 
-const getDataParams = computed(() => ({
-  area: areaTabs[activeAreaTabIndex.value],
-  order: sortTabs[activeSortTabIndex.value],
-  type: typeTabs[activeTypeTabIndex.value]
-}))
+const getDataParams = () => {
+    return {
+        area: areaTabs[activeAreaTabIndex.value],
+        order: sortTabs[activeSortTabIndex.value],
+        type: typeTabs[activeTypeTabIndex.value]
+    };
+};
+
+const scrollTarget = ref(null);
 
 const onGetMvs = ({ data, count }) => {
-  mvs.value = data
-  if (count) {
-    mvCount.value = count
-  }
-}
+    mvs.value = data;
+    if (count) {
+        mvCount.value = count;
+    }
+};
 
-onMounted(() => {
-  getAllMvs()
-})
 </script>
 
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -43,17 +44,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log'],
-      },
-      output: {
-        comments: false,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks(id) {
